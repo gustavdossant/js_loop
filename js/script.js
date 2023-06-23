ef560c651b87da4503afdb0cd8f51b7dacc5b018
gustavdossant <xspeedbr@gmail.com>
2023-06-23 16:09:33 -0300

    ....

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//  While
+let tabuada = 7;
+let contador = 0;
+
+
+while (contador <=10) {
+    let resultado = tabuada * contador;
+    console.log(tabuada + ' x ' + contador + ' = ' + resultado);
+    contador++;
+}
